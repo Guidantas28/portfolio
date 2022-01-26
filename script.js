08af7e9eb12e35c827335612a4ddbc180b91b059
Guilherme Dantas <guilhermedantas788@gmail.com>
2022-01-26 09:33:59 -0300

    finished html and css

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-fetch('https://api.github.com/users/Guidantas28')
+const name = document.getElementsByClassName('project-title')
+const description = document.getElementsByClassName('project-description')
+
+const apiurl = fetch('https://api.github.com/users/Guidantas28/repos')
     .then(response => response.json())
     .then(data => console.log(data))
